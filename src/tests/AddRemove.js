35a3320446b6e01ff8efa9799f3fadc90f1223d4
Abraha Kahsay <abrahak81@gmail.com>
2022-07-15 15:32:39 +0300

    Add remove js

---
+++ src/tests/AddRemove.js
@@ -0,0 +1,33 @@
+const tasks = [];
+function myTasks() {
+  return tasks;
+}
+const add = (textInputValue) => {
+  const TaskItem = {
+    description: textInputValue,
+    completed: false,
+    index: myTasks().length + 1,
+  };
+  const addedItem = TaskItem;
+  myTasks().push(addedItem);
+  // saveDataLocally(myTasks());
+  return myTasks();
+};
+const clearDiv = (div) => {
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+};
+const removeItemAt = (index) => {
+  myTasks().splice(index - 1, 1);
+  return myTasks();
+};
+const changeInput = (element) => {
+  const newDescription = element.value;
+  const elementIndex = element.getAttribute('index');
+  myTasks()[elementIndex - 1].description = newDescription;
+  // saveDataLocally(myTasks());
+};
+module.exports = {
+  add, myTasks, removeItemAt, clearDiv, changeInput,
+};
