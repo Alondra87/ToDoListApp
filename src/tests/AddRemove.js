8773252bfc2628b7cd444fa74a5657f0e7a1f189
Alondra87 <aa5887687@gmail.com>
2022-07-15 17:53:56 -0400

    Fix linter error

--- src/tests/AddRemove.js
+++ src/tests/AddRemove.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 const tasks = [];
 function myTasks() {
   return tasks;
@@ -12,20 +13,12 @@ const add = (textInputValue) => {
   myTasks().push(addedItem);
   return myTasks();
 };
-const clearDiv = (div) => {
-  while (div.firstChild) {
-    div.removeChild(div.firstChild);
-  }
-};
+
 const removeItemAt = (index) => {
   myTasks().splice(index - 1, 1);
   return myTasks();
 };
-const changeInput = (element) => {
-  const newDescription = element.value;
-  const elementIndex = element.getAttribute('index');
-  myTasks()[elementIndex - 1].description = newDescription;
-};
+
 module.exports = {
-  add, myTasks, removeItemAt, clearDiv, changeInput,
+  add, myTasks, removeItemAt,
 };
